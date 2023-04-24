007686540a48b9ac746acc5dc0304323e2f64f92
Manal <andnothingevrything@gmail.com>
2023-04-24 16:39:58 +0200

    added test.c

---
+++ test.c
@@ -0,0 +1,23 @@
+int seconde ( int x) {
+	int jus = x ;
+	bool pomme = true;
+	return jus;
+} ;
+
+void main ( void) {
+	int v = 10;
+	bool zed = true;
+	str w = "this is a string";
+	#int x = 10 + 3;
+	int x;
+	if (true){
+		x = seconde ( 5 );
+	} else {
+		x = seconde ( 3 );
+	};
+	#while ( 0 ){ x = v;};
+	return x;
+	
+	
+} ;
+
